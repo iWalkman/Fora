b9de625fac9dcc1a096529dc60646e6b4a22bb3c
Константин Фомин <konstantin.fomin@me.com>
2018-11-26 18:38:28 +0300

    Initial Commit

---
+++ ForaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForaTest
+//
+//  Created by Konstantin Fomin on 26/11/2018.
+//  Copyright © 2018 Konstantin Fomin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
